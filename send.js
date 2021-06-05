7b25ca8e6c6eede781863aa38b9e3f1628c2e264
mumustudio <mumushy850421@gmail.com>
2021-06-05 19:20:35 +0800

    update

--- send.js
+++ send.js
@@ -1,10 +1,10 @@
-/* eslint-env es6 */
-/* eslint-disable */
-var firebase;
-var config = {
-  databaseURL: "https://cantsleep-f8b26.firebaseio.com/"
-};
-firebase.initializeApp(config);
-
-var name = document.getElementById("name").nodeValue;
+/* eslint-env es6 */
+/* eslint-disable */
+var firebase;
+var config = {
+  databaseURL: "https://cantsleep-f8b26.firebaseio.com/"
+};
+firebase.initializeApp(config);
+
+var name = document.getElementById("name").nodeValue;
 alert(name);
